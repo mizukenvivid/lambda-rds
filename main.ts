829f3dd7c7a15085942a43c49b0ea2cb151a6ee6
mizukenvivid <kenji.mizuguchi@vivid-studio.jp>
2023-12-09 21:39:48 +0900

    feat: VPCをを追加

--- main.ts
+++ main.ts
@@ -1,11 +1,25 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { AwsProvider } from '@cdktf/provider-aws/lib/provider'
+import { Vpc } from "@cdktf/provider-aws/lib/vpc";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     // define resources here
+    new AwsProvider(this, 'aws', {
+      region: 'ap-northeast-1'
+    })
+
+    new Vpc(this, 'vpc', {
+      cidrBlock: '10.0.0.0/16',
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      tags: {
+        Name: 'cdktf-vpc'
+      }
+    })
   }
 }
 
